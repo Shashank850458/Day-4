b1b05802dfefbc59bf1612e0cc42ccff6d087b4a
shashank hebbar <86871679+Shashank850458@users.noreply.github.com>
2022-03-20 21:21:59 +0530

    Update question no 3 .js

--- question no 3 .js
+++ question no 3 .js
@@ -5,7 +5,7 @@ xhr.send();
 xhr.onload=function(){
    
     if(xhr.status>=200 && xhr.status<300){
-       //converts raw into js notation format 
+       
          var data=JSON.parse(xhr.response);
           
             for(var i=0; i<data.length; i++){
